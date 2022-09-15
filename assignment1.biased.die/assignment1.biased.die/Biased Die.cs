78d0931a6d96d7e568e60dd0a9836b9b4ad8ce68
Kav K <kav@KavsMacBookPro.local>
2022-09-15 23:36:32 +0700

    first commit

---
+++ assignment1.biased.die/assignment1.biased.die/Biased Die.cs
@@ -0,0 +1,101 @@
+﻿Random RNG = new Random();
+int[] Results = new int[600];
+int[] Count = new int[7];
+for (int i = 0; i < 600; i++)
+{
+    int RandomNumber = RNG.Next(1, 7);
+    Results[i] = RandomNumber;
+}
+for (int i = 0; i < 600; i++)
+{
+    if (i % 100 == 0)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Person " + (i+100)/100);
+    }
+    Console.Write(Results[i] + "  ");
+    if ((i+1)%10 == 0)
+    {
+        Console.WriteLine("");
+    }  
+}
+for(int i = 0; i < Results.Length; i++)
+{
+    int num = Results[i];
+    Count[num] = Count[num] + 1;
+}
+for(int i = 1; i < Count.Length; i++)
+{
+    Console.WriteLine("Number of " + i + "s = " + Count[i]);
+}
+for(int i = 1; i < Count.Length; i++)
+{
+    int Number = Count[i];
+    if(Number > 125)
+    {
+        Console.WriteLine("Biased Towards: " + i);
+    }
+    if (Number < 125 && Number > 100)
+    {
+        Console.WriteLine("Slightly Biased Towards: " + i);
+    }
+    if (Number < 100 && Number > 75)
+    {
+        Console.WriteLine("Slightly Not Biased Towards: " + i);
+    }
+    if(Number == 100)
+    {
+        Console.WriteLine("Not Biased(at all)Towards: " + i);
+    }
+    if(Number < 75)
+    {
+        Console.WriteLine("Not Biased Towards: " + i);
+    }
+}
+//Extension 1
+for(int i = 1; i < Count.Length; i++)
+{
+    for(int x = 1; x < Count.Length - 1; x++)
+    {
+        if (Count[x] < Count[x+1])
+        {
+            int c = Count[x+1];
+            Count[x+1] = Count[x];
+            Count[x] = c;
+        }
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Descending Order");
+for (int i = 1; i < Count.Length; i++)
+{
+    Console.WriteLine(Count[i]);
+}
+//Extension 2
+int even = 0;
+int odd = 0;
+for(int i = 0; i < Results.Length; i++)
+{
+    if (Results[i]%2 == 0)
+    {
+        even++;
+    }
+    else
+    {
+        odd++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Number of Even = " + even);
+Console.WriteLine("Number of Odd = " + odd);
+//Extension 3
+for(int i = 0; i < Count.Length;i++)
+{
+    double Number = (double)(Count[i] / (double)Count[1]) * 100;
+
+    for(int x = 1; x <= Number; x++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
---
+++ assignment2.roulette/assignment2.roulette/Program.cs
@@ -0,0 +1,92 @@
+﻿Random RNG = new Random();
+int Start = 100;
+int Bet = 0;
+int Number = 0;
+string Colour = "";
+while (Start !>= 0)
+{
+    Console.WriteLine("Place your bets, you have " + Start + "$ available");
+    string Input = Console.ReadLine();
+
+    Bet = Convert.ToInt32(Input);
+    if(Bet > Start || Bet <= 0)
+    {
+        Console.WriteLine("Invalid");
+        break;
+    }
+    Console.WriteLine("Do you want to choose a number or a color?");
+    string Choice = Console.ReadLine();
+    if(Choice == "Number" || Choice == "number" || Choice == "NUMBER")
+    {
+        Console.WriteLine("Enter a number between 0 and 36");
+        string Num = Console.ReadLine();
+        Number = Convert.ToInt32(Num);
+        
+    }
+    else if(Choice == "colour" || Choice == "Colour" || Choice == "COLOUR" || Choice == "color" || Choice == "Color" || Choice == "COLOR")
+    {
+        Console.WriteLine("Choose Red or Black");
+        Colour = Console.ReadLine();
+    }
+    else
+    {
+        Console.WriteLine("Invalid");
+        break;
+    }
+    int RandomNumber = RNG.Next(0, 37);
+    Console.WriteLine(RandomNumber);
+    if(Choice == "colour" || Choice == "Colour" || Choice == "COLOUR" || Choice == "color" || Choice == "Color" || Choice == "COLOR")
+    {
+        if (Colour == "Black" || Colour == "black" || Colour == "BLACK")
+        {
+
+            if (RandomNumber % 2 == 0)
+            {
+                Start = Bet * 2 + (Start - Bet);
+                Console.WriteLine("Money left = " + Start + "$");
+            }
+            else
+            {
+                Start = Start - Bet;
+                Console.WriteLine("Money left = " + Start + "$");
+            }
+        }
+        else if (Colour == "Red" || Colour == "red" || Colour == "RED")
+        {
+            if (RandomNumber % 2 == 0)
+            {
+                Start = Start - Bet;
+                Console.WriteLine("Money left = " + Start + "$");
+            }
+            else
+            {
+                Start = Bet * 2 + (Start - Bet);
+                Console.WriteLine("Money left = " + Start + "$");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid");
+            break;
+        }
+    }
+    else if(Choice == "number" || Choice == "NUMBER" || Choice == "Number")
+    {
+        if (RandomNumber == Number)
+        {
+            Start = Bet * 36 + (Start - Bet);
+            Console.WriteLine("Money left = " + Start + "$");
+        }
+        else
+        {
+            Start = Start - Bet;
+            Console.WriteLine("Money left = " + Start + "$");
+        }
+    }
+
+
+    if (Start == 0)
+    {
+        break;
+    }
+}
